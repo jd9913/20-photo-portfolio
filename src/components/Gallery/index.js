8763bcf0e15415ee252da09982cd0f313313335c
Jennifer Schlierman <jd9913@gmail.com>
2020-12-13 19:29:00 -0700

    adding photo list

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,29 +1,18 @@
 import React from 'react';
+import PhotoList from './PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
 
+function Gallery ({ currentCategory }) {
 
-function Gallery(){
+  const { name, description } = currentCategory;
 
-const currentCategory={
-    name: "commercial",
-    description: "Photos of grocery stores, food trucks, and other commercial projects"
-};
-
-
-    return(
-        <section>
-            <h1>{ capitalizeFirstLetter(currentCategory.name) }</h1>
-            <p>{ currentCategory.name }</p>
-            <div className="flex-row">
-                <img src={ photo }
-                alt="Commercial Example"
-                className="img-thumbnail mx-1"
-                />
-            </div>
-        </section>
-    );
+  return (
+    <section>
+      <h1 data-testid="h1tag" > { capitalizeFirstLetter (name) }</h1>
+      <p>{ description }</p>
+      <PhotoList  />
+    </section>
+  );
 }
 
 export default Gallery;
-
